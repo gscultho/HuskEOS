ffa4685f6c142eef9688247a12d76ede49da3a3f
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2019-07-17 21:38:36 -0400

    7_17
    Added comments

--- huskEOS/Global/Header/rtos_cfg.h
+++ huskEOS/Global/Header/rtos_cfg.h
@@ -6,7 +6,7 @@
 /*  All rights reserved.                                                 */
 /*************************************************************************/
 
-#ifndef rtos_cfg_h /* Protection from declaring more than once */
+#ifndef rtos_cfg_h 
 #define rtos_cfg_h
 
 #include "cpu_defs.h"
@@ -19,18 +19,18 @@
 
 /* Application */
 #define RTOS_CONFIG_TASK_STACK_SIZE                 (100)                /* Does not need to be used, each task can have its own stack size */
-#define RTOS_CONFIG_BG_TASK_STACK_SIZE              (50)
+#define RTOS_CONFIG_BG_TASK_STACK_SIZE              (50)                 /* Stack size for background task if enabled */
 #define RTOS_CONFIG_CALC_TASK_CPU_LOAD              (RTOS_CONFIG_TRUE)   /* Can only be enabled if RTOS_CONFIG_BG_TASK and RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP enabled */
 
 
 /* Scheduling */
-#define RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP    (RTOS_CONFIG_TRUE)   /* Can only be enabled ifRTOS_CONFIG_BG_TASK is enabled */
-#define RTOS_CONFIG_ENABLE_STACK_OVERFLOW_DETECT    (RTOS_CONFIG_TRUE)
-#define RTOS_CONFIG_BG_TASK                         (RTOS_CONFIG_TRUE)
-#define RTOS_CONFIG_MAX_NUM_TASKS                   (6)
-#define RTOS_CONFIG_PRESLEEP_FUNC                   (RTOS_CONFIG_TRUE)
-#define RTOS_CONFIG_POSTSLEEP_FUNC                  (RTOS_CONFIG_TRUE)
-#define RTOS_CONFIG_APP_IDLE_TASK                   (RTOS_CONFIG_TRUE)
+#define RTOS_CONFIG_MAX_NUM_TASKS                   (6)                  /* This number of TCBs will be allocated at compile-time, plus any others used by OS */
+
+#define RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP    (RTOS_CONFIG_TRUE)   /* Can only be used if RTOS_CONFIG_BG_TASK is enabled */
+#define RTOS_CONFIG_ENABLE_STACK_OVERFLOW_DETECT    (RTOS_CONFIG_TRUE)   /* Can only be used if RTOS_CONFIG_BG_TASK is enabled */
+#define RTOS_CONFIG_BG_TASK                         (RTOS_CONFIG_TRUE)   /* OS will handle background activities and then put CPU to sleep when idle */
+#define RTOS_CONFIG_PRESLEEP_FUNC                   (RTOS_CONFIG_TRUE)   /* If enabled, hook function app_OSPreSleepFcn() can be defined in application. */
+#define RTOS_CONFIG_POSTSLEEP_FUNC                  (RTOS_CONFIG_TRUE)   /* If enabled, hook function app_OSPostSleepFcn() can be defined in application. */
 
 /* Mailbox */
 #define RTOS_CFG_OS_MAILBOX_ENABLED                 (RTOS_CONFIG_TRUE)
@@ -69,4 +69,5 @@
 /*************************************************************************/
 
 
-#endif /* End conditional declaration for rtos_cfg_h */
+#endif 
+
