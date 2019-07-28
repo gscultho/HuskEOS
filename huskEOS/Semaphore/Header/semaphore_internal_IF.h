c2d3c00e0f19cf973e595ec09fc838adad261895
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2019-07-27 21:54:40 -0400

    7_27_02

--- huskEOS/Semaphore/Header/semaphore_internal_IF.h
+++ huskEOS/Semaphore/Header/semaphore_internal_IF.h
@@ -5,7 +5,7 @@
 /*  Copyright © 2019 Garrett Sculthorpe. All rights reserved.            */
 /*************************************************************************/
 
-#ifndef semaphore_internal_IF_h /* Protection from declaring more than once */
+#ifndef semaphore_internal_IF_h 
 #define semaphore_internal_IF_h
 
 #include "cpu_defs.h"
@@ -15,15 +15,17 @@
 /*  Definitions                                                          */
 /*************************************************************************/
 #define SEMA_MAX_NUM_BLOCKED           (RTOS_CFG_NUM_BLOCKED_TASKS_SEMA)
-
-
+  
 /*************************************************************************/
 /*  Data Types                                                           */
 /*************************************************************************/
-typedef struct
+struct listNode; /* Forward declaration. Defined in "listMgr_internal.h" */
+
+typedef struct Semaphore
 {
-  S1    sema;
-  U1    blockedTasks[SEMA_MAX_NUM_BLOCKED];
+  S1               sema;
+  struct ListNode  blockedTasks[SEMA_MAX_NUM_BLOCKED];
+  struct ListNode* blockedListHead;  
 }
 Semaphore;
 
@@ -31,12 +33,21 @@ Semaphore;
 /*************************************************************************/
 /*  Public Functions                                                     */
 /*************************************************************************/
-void vd_sema_blockedTimeout(Semaphore* sema, U1 taskID);
 
+/*************************************************************************/
+/*  Function Name: vd_sema_blockedTimeout                                */
+/*  Purpose:       API for scheduler to call when sleeping task times out*/
+/*  Arguments:     Semaphore* semaphore:                                 */
+/*                     Pointer to semaphore.                             */
+/*                 Sch_Task* taskTCB:                                    */
+/*                     Pointer to TCB of blocked task.                   */
+/*  Return:        N/A                                                   */
+/*************************************************************************/
+void vd_sema_blockedTimeout(struct Semaphore* semaphore, struct Sch_Task* taskTCB);
 
 /*************************************************************************/
 /*  Global Variables                                                     */
 /*************************************************************************/
 
 
-#endif /* End conditional declaration for semaphore_internal_IF_h */
+#endif 
