266c2c8c9ae58b0541fba980fb034b01ebed0533
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2019-08-02 19:48:41 -0400

    8_2

--- huskEOS/Flags/Header/flags_internal_IF.h
+++ huskEOS/Flags/Header/flags_internal_IF.h
@@ -5,7 +5,7 @@
 /*  Copyright © 2019 Garrett Sculthorpe. All rights reserved.            */
 /*************************************************************************/
 
-#ifndef flags_internal_IF_h /* Protection from declaring more than once */
+#ifndef flags_internal_IF_h 
 #define flags_internal_IF_h
 
 #include "cpu_defs.h"
@@ -14,17 +14,26 @@
 /*************************************************************************/
 /*  Definitions                                                          */
 /*************************************************************************/
-
+#define FLAGS_MAX_NUM_TASKS_PENDING       (RTOS_CFG_MAX_NUM_TASKS_PEND_FLAGS)
 
 
 /*************************************************************************/
 /*  Data Types                                                           */
 /*************************************************************************/
-typedef struct 
+struct Sch_Task; /* Forward declaration. See definition in sch_internal_IF.h */
+
+typedef struct TasksPending
 {
-  U1 flags;
-  U1 pendedTaskID;
-  U1 pendEvent;
+  U1               event;
+  struct Sch_Task* tcb;
+  U1               eventPendType;
+}
+TasksPending;
+
+typedef struct FlagsObj
+{
+  U1            flags;
+  TasksPending  pendingList[FLAGS_MAX_NUM_TASKS_PENDING];
 }
 FlagsObj;
 
@@ -32,7 +41,16 @@ FlagsObj;
 /*************************************************************************/
 /*  Public Functions                                                     */
 /*************************************************************************/
-void vd_flags_pendTimeout(FlagsObj* flags);
+/*************************************************************************/
+/*  Function Name: vd_flags_pendTimeout                                  */
+/*  Purpose:       Timeout hander for pending task. Called by scheduler. */
+/*  Arguments:     FlagsObj* flags:                                      */
+/*                      Pointer to flags object.                         */
+/*                 Sch_Task pendingTCB:                                  */
+/*                      Pointer to timed out task's TCB.                 */
+/*  Return:        N/A                                                   */
+/*************************************************************************/
+void vd_flags_pendTimeout(struct FlagsObj* flags, struct Sch_Task* pendingTCB);
 
 
 /*************************************************************************/
@@ -40,4 +58,4 @@ void vd_flags_pendTimeout(FlagsObj* flags);
 /*************************************************************************/
 
 
-#endif /* End conditional declaration for flags_internal_IF_h */
+#endif 
