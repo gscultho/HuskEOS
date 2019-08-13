206fe3a96f24c3d361bc3aa1437fec6782abe641
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2019-08-12 21:37:09 -0400

    8_12

--- huskEOS/Queue/Header/queue.h
+++ huskEOS/Queue/Header/queue.h
@@ -5,11 +5,9 @@
 /*  Copyright © 2019 Garrett Sculthorpe. All rights reserved.            */
 /*************************************************************************/
 
-#ifndef queue_h /* Protection from declaring more than once */
+#ifndef queue_h 
 #if(RTOS_CFG_OS_QUEUE_ENABLED == RTOS_CONFIG_TRUE)
 #define queue_h
-
-#include "queue_internal_IF.h"
 
 /*************************************************************************/
 /*  Definitions                                                          */
@@ -23,7 +21,8 @@
 #define FIFO_SUCCESS                         (1)
 
 /* API error codes */
-#define FIFO_ERR_NO_QUEUE_AVAILABLE          (0)
+#define FIFO_ERR_NO_ERROR                    (0)
+#define FIFO_ERR_NO_QUEUE_AVAILABLE          (3)
 #define FIFO_ERR_QUEUE_OUT_OF_RANGE          (255)
 #define FIFO_ERR_QUEUE_FULL                  (1)
 #define FIFO_ERR_QUEUE_EMPTY                 (1)
@@ -32,11 +31,12 @@
 /*************************************************************************/
 /*  Data Types                                                           */
 /*************************************************************************/
-
+typedef struct Queue OSQueue;
 
 /*************************************************************************/
 /*  Public Functions                                                     */
 /*************************************************************************/
+U1    u1_OSqueue_init(Q_MEM* queueStart, U4 queueLength);
 Q_MEM data_OSqueue_get(U1 queueNum, U4 blockPeriod, U1* error);
 U1    u1_OSqueue_put(U1 queueNum, U4 blockPeriod, Q_MEM message, U1* error);
 U1    u1_OSqueue_flushFifo(U1 queueNum, U1* error);
@@ -51,4 +51,4 @@ U1    u1_OSqueue_getSts(U1 queueNum, U1* error);
 #warning "QUEUE MODULE NOT ENABLED"
 
 #endif /* Conditional compile */
-#endif /* End conditional declaration for queue_h */
+#endif 
