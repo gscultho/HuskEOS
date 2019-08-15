5f0677ef613fb888208779bb3aaa528c06df20ec
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2019-08-14 23:46:41 -0400

    8_14_01

--- huskEOS/Global/Header/rtos_cfg.h
+++ huskEOS/Global/Header/rtos_cfg.h
@@ -20,7 +20,7 @@
 /* Application */
 #define RTOS_CONFIG_TASK_STACK_SIZE                 (200)                /* Does not need to be used, each task can have its own stack size */
 #define RTOS_CONFIG_BG_TASK_STACK_SIZE              (50)                 /* Stack size for background task if enabled */
-#define RTOS_CONFIG_CALC_TASK_CPU_LOAD              (RTOS_CONFIG_TRUE)   /* Can only be enabled if RTOS_CONFIG_BG_TASK and RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP enabled */
+#define RTOS_CONFIG_CALC_TASK_CPU_LOAD              (RTOS_CONFIG_FALSE)  /* Can only be enabled if RTOS_CONFIG_BG_TASK and RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP enabled */
 
 
 /* Scheduling */
@@ -34,25 +34,30 @@
 
 /* Mailbox */
 #define RTOS_CFG_OS_MAILBOX_ENABLED                 (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_MAILBOX                        (3)
+#define RTOS_CFG_NUM_MAILBOX                        (3)                   /* Number of mailboxes available in run-time. */
 #define RTOS_CFG_MBOX_DATA                          U4                    /* Data type for mailbox */
 
 /* Message Queues */
 #define RTOS_CFG_OS_QUEUE_ENABLED                   (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_FIFO                           (3)
-#define RTOS_CFG_BUFFER_LENGTH                      (10)
-#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_FIFO         (3)
-#define RTOS_CFG_BUFFER_DATA                        U4
+#define RTOS_CFG_NUM_FIFO                           (3)                   /* Number of FIFOs available in run-time. */             
+#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_FIFO         (3)                   /* Maximum number of tasks that can block on each FIFO. */
+#define RTOS_CFG_BUFFER_DATA                        U4                    /* Type of data used in the buffers. */
 
 /* Semaphores */
 #define RTOS_CFG_OS_SEMAPHORE_ENABLED               (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_SEMAPHORES                     (2)
-#define RTOS_CFG_NUM_BLOCKED_TASKS_SEMA             (3)
+#define RTOS_CFG_NUM_SEMAPHORES                     (2)                   /* Number of semaphores available in run-time. */
+#define RTOS_CFG_NUM_BLOCKED_TASKS_SEMA             (3)                   /* Maximum number of tasks that can block on each FIFO. */
 
 /* Flags */
 #define RTOS_CFG_OS_FLAGS_ENABLED                   (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_FLAG_OBJECTS                   (2)
+#define RTOS_CFG_NUM_FLAG_OBJECTS                   (2)                   /* Number of flag objects available in run-time. */
 #define RTOS_CFG_MAX_NUM_TASKS_PEND_FLAGS           (2)                   /* Maximum number of tasks that can pend on flags object. */
+
+/* Mutex */
+#define RTOS_CFG_OS_MUTEX_ENABLED                   (RTOS_CONFIG_TRUE)   
+#define RTOS_CFG_MAX_NUM_MUTEX                      (2)                   /* Number of mutexes available in run-time. */
+#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_MUTEX        (2)                   /* Number of tasks that can block on each mutex. */
+
 
 /* Memory */
 #define RTOS_CFG_MAX_NUM_MEM_BLOCKS                 (16)
