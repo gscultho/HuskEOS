7f7fd61667d21618b7092c65a485179f1ed3a5f8
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2019-08-27 19:57:26 -0400

    8_27

--- huskEOS/Flags/Header/flags.h
+++ huskEOS/Flags/Header/flags.h
@@ -2,12 +2,15 @@
 /*  File Name:  flags.h                                                  */
 /*  Purpose:    Public header file for flags module.                     */
 /*  Created by: Garrett Sculthorpe on 3/24/19                            */
-/*  Copyright © 2019 Garrett Sculthorpe. All rights reserved.            */
+/*  Copyright © 2019 Garrett Sculthorpe and Darren Cicala.               */
+/*              All rights reserved.                                     */
 /*************************************************************************/
 
 #ifndef flags_h 
+#if(RTOS_CFG_OS_FLAGS_ENABLED == RTOS_CONFIG_TRUE)
 #define flags_h
 
+#include "rtos_cfg.h"
 
 /*************************************************************************/
 /*  Definitions                                                          */
@@ -34,19 +37,19 @@ typedef struct FlagsObj OSFlagsObj;
 /*************************************************************************/
 /*  Function Name: u1_OSflags_init                                       */
 /*  Purpose:       Initialize flags object.                              */
-/*  Arguments:     FlagsObj** flags:                                     */
+/*  Arguments:     OSFlagsObj** flags:                                   */
 /*                        Address of flags object.                       */
 /*                 U1 flagInitValues:                                    */
 /*                        Initial values for flags.                      */
 /*  Return:        U1: FLAGS_NO_OBJ_AVAILABLE     OR                     */
 /*                     FLAGS_INIT_SUCCESS                                */
 /*************************************************************************/
-U1 u1_OSflags_init(struct FlagsObj** flags, U1 flagInitValues);
+U1 u1_OSflags_init(OSFlagsObj** flags, U1 flagInitValues);
 
 /*************************************************************************/
 /*  Function Name: u1_OSflags_postFlags                                  */
 /*  Purpose:       Write to flags object as specified.                   */
-/*  Arguments:     FlagsObj* flags:                                      */
+/*  Arguments:     OSFlagsObj* flags:                                    */
 /*                    Pointer to flags object.                           */
 /*                 U1 flagMask:                                          */
 /*                    Mask to set/clear.                                 */
@@ -56,7 +59,7 @@ U1 u1_OSflags_init(struct FlagsObj** flags, U1 flagInitValues);
 /*  Return:        U4 FLAGS_WRITE_COMMAND_INVALID     OR                 */
 /*                    FLAGS_WRITE_SUCCESS                                */
 /*************************************************************************/
-U1 u1_OSflags_postFlags(struct FlagsObj* flags, U1 flagMask, U1 set_clear); 
+U1 u1_OSflags_postFlags(OSFlagsObj* flags, U1 flagMask, U1 set_clear); 
 
 /*************************************************************************/
 /*  Function Name: u1_OSflags_pendOnFlags                                */
@@ -64,7 +67,7 @@ U1 u1_OSflags_postFlags(struct FlagsObj* flags, U1 flagMask, U1 set_clear);
 /*                 it can retrieve the waking event by calling           */
 /*                 u1_OSsch_getReasonForWakeup()                         */
 /*                                                                       */
-/*  Arguments:     FlagsObj* flags:                                      */
+/*  Arguments:     OSFlagsObj* flags:                                    */
 /*                      Pointer to flags object.                         */
 /*                 U1 eventMask:                                         */
 /*                      Event that will cause wakeup.                    */
@@ -77,39 +80,42 @@ U1 u1_OSflags_postFlags(struct FlagsObj* flags, U1 flagMask, U1 set_clear);
 /*  Return:        U1: FLAGS_PEND_LIST_FULL     OR                       */
 /*                     FLAGS_PEND_SUCCESS                                */
 /*************************************************************************/
-U1 u1_OSflags_pendOnFlags(struct FlagsObj* flags, U1 eventMask, U4 timeOut, U1 eventType);
+U1 u1_OSflags_pendOnFlags(OSFlagsObj* flags, U1 eventMask, U4 timeOut, U1 eventType);
 
 /*************************************************************************/
 /*  Function Name: vd_OSflags_reset                                      */
 /*  Purpose:       Reset flags object to init state.                     */
-/*  Arguments:     Flags* flags:                                         */
+/*  Arguments:     OSFlagsObj* flags:                                    */
 /*                     Pointer to flags object.                          */
 /*  Return:        N/A                                                   */
 /*************************************************************************/
-void vd_OSflags_reset(struct FlagsObj* flags);
+void vd_OSflags_reset(OSFlagsObj* flags);
 
 /*************************************************************************/
 /*  Function Name: vd_OSflags_clearAll                                   */
 /*  Purpose:       Clear all flags.                                      */
-/*  Arguments:     Flags* flags:                                         */
+/*  Arguments:     OSFlagsObj* flags:                                    */
 /*                     Pointer to flags object.                          */
 /*  Return:        N/A                                                   */
 /*************************************************************************/
-void vd_OSflags_clearAll(struct FlagsObj* flags);
+void vd_OSflags_clearAll(OSFlagsObj* flags);
 
 /*************************************************************************/
 /*  Function Name: u1_flags_checkFlags                                   */
 /*  Purpose:       Check flags but do not modify.                        */
-/*  Arguments:     Flags* flags:                                         */
+/*  Arguments:     OSFlagsObj* flags:                                    */
 /*                     Pointer to flags object.                          */
 /*  Return:        U1 u1_t_returnVal:                                    */
 /*                    Value of flags at time they were read.             */
 /*************************************************************************/
-U1 u1_OSflags_checkFlags(struct FlagsObj* flags);
+U1 u1_OSflags_checkFlags(OSFlagsObj* flags);
 
 /*************************************************************************/
 /*  Global Variables                                                     */
 /*************************************************************************/
 
+#else
+#warning "FLAGS MODULE NOT ENABLED"
 
+#endif /* Conditional compile */
 #endif
