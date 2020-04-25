7f415240495d35753124d89e73758d163ba94f8e
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2020-04-25 19:27:35 -0400

    Update startup.s

--- App/Startup/ASM/startup.s
+++ App/Startup/ASM/startup.s
@@ -1,26 +1,3 @@
-; <<< Use Configuration Wizard in Context Menu >>>
-;******************************************************************************
-;
-; startup_rvmdk.S - Startup code for use with Keil's uVision.
-;
-; Copyright (c) 2012 Texas Instruments Incorporated.  All rights reserved.
-; Software License Agreement
-; 
-; Texas Instruments (TI) is supplying this software for use solely and
-; exclusively on TI's microcontroller products. The software is owned by
-; TI and/or its suppliers, and is protected under applicable copyright
-; laws. You may not combine this software with "viral" open-source
-; software in order to form a larger program.
-; 
-; THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-; NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-; NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-; A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-; CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-; DAMAGES, FOR ANY REASON WHATSOEVER.
-; 
-; This is part of revision 9453 of the EK-LM4F120XL Firmware Package.
-;
 ;******************************************************************************
 ;
 ; <o> Stack Size (in Bytes) <0x0-0xFFFFFFFF:8>
