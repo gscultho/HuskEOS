b1abeb8d8ae58959e74eb0c6dac45f5ac89e66ea
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2020-04-26 02:08:03 -0400

    Update app.c

--- App/Source/app.c
+++ App/Source/app.c
@@ -2,7 +2,7 @@
 /* Starter app file for OS with three threads set up to run. Include     */
 /* header files from other OS modules to use them and create new threads */
 /* by following the code below for reference. All module design and API  */
-/* information can be found in documetnation in repository.              */
+/* information can be found in documentation in repository.              */
 /*************************************************************************/
 
 /* OS includes */
