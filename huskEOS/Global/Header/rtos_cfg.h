25a800868565cbc50b9aee5007dfad2e44311176
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2020-04-27 21:00:03 -0400

    4_27

--- huskEOS/Global/Header/rtos_cfg.h
+++ huskEOS/Global/Header/rtos_cfg.h
@@ -57,8 +57,10 @@
 
 
 /* Memory */
-#define RTOS_CFG_OS_MEM_ENABLED                     (RTOS_CONFIG_FALSE)
-#define RTOS_CFG_MAX_NUM_MEM_BLOCKS                 (16)                  /* Number of memory blocks available in run-time. */
+#define RTOS_CFG_OS_MEM_ENABLED                     (RTOS_CONFIG_TRUE)
+#define RTOS_CFG_MAX_NUM_MEM_PARTITIONS             (16)                  /* Maximum number of memory partitions available in run-time. */
+#define RTOS_CFG_MAX_NUM_MEM_BLOCKS                 (16)                  /* Maximum number of blocks that can be contained within a partition. */
+#define RTOS_CFG_MAX_MEM_BLOCK_SIZE                 (64)                  /* Maximum memory block size. */
 
 /* I/O */
 #define PART_TM4C123GH6PM 1
