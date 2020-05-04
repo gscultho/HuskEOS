c54c6156b30dbe072f4b4dc29ccf67efeb5516c5
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2020-05-04 17:20:06 -0400

    5_4

--- huskEOS/Global/Header/rtos_cfg.h
+++ huskEOS/Global/Header/rtos_cfg.h
@@ -22,7 +22,7 @@
 #define RTOS_CONFIG_CALC_TASK_CPU_LOAD              (RTOS_CONFIG_FALSE)  /* Can only be enabled if RTOS_CONFIG_BG_TASK and RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP enabled */
 
 /* Scheduling */
-#define RTOS_CONFIG_MAX_NUM_TASKS                   (6)                  /* This number of TCBs will be allocated at compile-time, plus any others used by OS */
+#define RTOS_CONFIG_MAX_NUM_TASKS                   (3)                  /* This number of TCBs will be allocated at compile-time, plus any others used by OS */
                                                                          /* Available priorities are 0 - 0xEF with 0 being highest priority. */
 #define RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP    (RTOS_CONFIG_TRUE)   /* Can only be used if RTOS_CONFIG_BG_TASK is enabled */
 #define RTOS_CONFIG_ENABLE_STACK_OVERFLOW_DETECT    (RTOS_CONFIG_TRUE)   /* Can only be used if RTOS_CONFIG_BG_TASK is enabled */
@@ -30,37 +30,37 @@
 #define RTOS_CONFIG_POSTSLEEP_FUNC                  (RTOS_CONFIG_FALSE)  /* If enabled, hook function app_OSPostSleepFcn() can be defined in application. */
 
 /* Mailbox */
-#define RTOS_CFG_OS_MAILBOX_ENABLED                 (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_MAILBOX                        (3)                   /* Number of mailboxes available in run-time. */
+#define RTOS_CFG_OS_MAILBOX_ENABLED                 (RTOS_CONFIG_FALSE)
+#define RTOS_CFG_NUM_MAILBOX                        (0)                   /* Number of mailboxes available in run-time. */
 #define RTOS_CFG_MBOX_DATA                          U4                    /* Data type for mailbox */
 
 /* Message Queues */
-#define RTOS_CFG_OS_QUEUE_ENABLED                   (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_FIFO                           (3)                   /* Number of FIFOs available in run-time. */             
-#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_FIFO         (3)                   /* Maximum number of tasks that can block on each FIFO. */
+#define RTOS_CFG_OS_QUEUE_ENABLED                   (RTOS_CONFIG_FALSE)
+#define RTOS_CFG_NUM_FIFO                           (0)                   /* Number of FIFOs available in run-time. */             
+#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_FIFO         (0)                   /* Maximum number of tasks that can block on each FIFO. */
 #define RTOS_CFG_BUFFER_DATA                        U4                    /* Type of data used in the buffers. */
 
 /* Semaphores */
-#define RTOS_CFG_OS_SEMAPHORE_ENABLED               (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_SEMAPHORES                     (2)                   /* Number of semaphores available in run-time. */
-#define RTOS_CFG_NUM_BLOCKED_TASKS_SEMA             (3)                   /* Maximum number of tasks that can block on each FIFO. */
+#define RTOS_CFG_OS_SEMAPHORE_ENABLED               (RTOS_CONFIG_FALSE)
+#define RTOS_CFG_NUM_SEMAPHORES                     (0)                   /* Number of semaphores available in run-time. */
+#define RTOS_CFG_NUM_BLOCKED_TASKS_SEMA             (0)                   /* Maximum number of tasks that can block on each FIFO. */
 
 /* Flags */
-#define RTOS_CFG_OS_FLAGS_ENABLED                   (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_NUM_FLAG_OBJECTS                   (2)                   /* Number of flag objects available in run-time. */
-#define RTOS_CFG_MAX_NUM_TASKS_PEND_FLAGS           (2)                   /* Maximum number of tasks that can pend on flags object. */
+#define RTOS_CFG_OS_FLAGS_ENABLED                   (RTOS_CONFIG_FALSE)
+#define RTOS_CFG_NUM_FLAG_OBJECTS                   (0)                   /* Number of flag objects available in run-time. */
+#define RTOS_CFG_MAX_NUM_TASKS_PEND_FLAGS           (0)                   /* Maximum number of tasks that can pend on flags object. */
 
 /* Mutex */
-#define RTOS_CFG_OS_MUTEX_ENABLED                   (RTOS_CONFIG_TRUE)   
-#define RTOS_CFG_MAX_NUM_MUTEX                      (2)                   /* Number of mutexes available in run-time. */
-#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_MUTEX        (2)                   /* Number of tasks that can block on each mutex. */
+#define RTOS_CFG_OS_MUTEX_ENABLED                   (RTOS_CONFIG_FALSE)   
+#define RTOS_CFG_MAX_NUM_MUTEX                      (0)                   /* Number of mutexes available in run-time. */
+#define RTOS_CFG_MAX_NUM_BLOCKED_TASKS_MUTEX        (0)                   /* Number of tasks that can block on each mutex. */
 
 
 /* Memory */
-#define RTOS_CFG_OS_MEM_ENABLED                     (RTOS_CONFIG_TRUE)
-#define RTOS_CFG_MAX_NUM_MEM_PARTITIONS             (16)                  /* Maximum number of memory partitions available in run-time. */
-#define RTOS_CFG_MAX_NUM_MEM_BLOCKS                 (16)                  /* Maximum number of blocks that can be contained within a partition. */
-#define RTOS_CFG_MAX_MEM_BLOCK_SIZE                 (64)                  /* Maximum memory block size. */
+#define RTOS_CFG_OS_MEM_ENABLED                     (RTOS_CONFIG_FALSE)
+#define RTOS_CFG_MAX_NUM_MEM_PARTITIONS             (0)                  /* Maximum number of memory partitions available in run-time. */
+#define RTOS_CFG_MAX_NUM_MEM_BLOCKS                 (0)                  /* Maximum number of blocks that can be contained within a partition. */
+#define RTOS_CFG_MAX_MEM_BLOCK_SIZE                 (0)                  /* Maximum memory block size. */
 
 /* I/O */
 #define PART_TM4C123GH6PM 1
