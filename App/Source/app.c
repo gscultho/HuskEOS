cab765799e927d587d8535db6e98767bbecffd57
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2020-05-04 21:50:03 -0400

    5_4

--- App/Source/app.c
+++ App/Source/app.c
@@ -5,6 +5,7 @@
 /* information can be found in documentation in repository.              */
 /*************************************************************************/
 
+
 /* OS includes */
 #include "sch.h"
 
@@ -13,16 +14,16 @@
 /*************************************************************************/
 /*  Definitions                                                          */
 /*************************************************************************/
-#define OS_TICK_MS             (1)
-#define OS_TASK_STACK_SIZE     (200)
+#define APP_TICK_MS            (1)
+#define APP_TASK_STACK_SIZE    (1)
 
-#define OS_TASK1_PRIO          (1)
-#define OS_TASK2_PRIO          (2)
-#define OS_TASK3_PRIO          (3)
+#define APP_TASK1_PRIO         (0)
+#define APP_TASK2_PRIO         (1)
+#define APP_TASK3_PRIO         (2)
 
-#define OS_TASK1_PERIOD        (1)
-#define OS_TASK2_PERIOD        (5)
-#define OS_TASK3_PERIOD        (10)
+#define APP_TASK1_PERIOD       (1)
+#define APP_TASK2_PERIOD       (5)
+#define APP_TASK3_PERIOD       (10)
 
 /*************************************************************************/
 /*  Private Function Prototypes                                          */
@@ -34,9 +35,9 @@ static void app_task3(void);
 /*************************************************************************/
 /*  Global Variables                                                     */
 /*************************************************************************/
-static U4 u4_taskStack [OS_TASK_STACK_SIZE];
-static U4 u4_taskStack2[OS_TASK_STACK_SIZE];
-static U4 u4_taskStack3[OS_TASK_STACK_SIZE];
+static U4 u4_taskStack [APP_TASK_STACK_SIZE];
+static U4 u4_taskStack2[APP_TASK_STACK_SIZE];
+static U4 u4_taskStack3[APP_TASK_STACK_SIZE];
 
 /*************************************************************************/
 
@@ -50,26 +51,26 @@ static U4 u4_taskStack3[OS_TASK_STACK_SIZE];
 int main()
 {
   /* Initialize OS with tick period. */
-  vd_OS_init(OS_TICK_MS);
+  vd_OS_init(APP_TICK_MS);
   
   /* Init task 1. */
   u1_OSsch_createTask(&app_task1,                             /* Pointer to function definition. */
-                     &u4_taskStack[OS_TASK_STACK_SIZE - 1],   /* Pointer to highest memory address in stack. */
-                     sizeof(u4_taskStack),                    /* Size of stack. */
-                     OS_TASK1_PRIO,                           /* Priority of task. */
-                     OS_TASK1_PRIO);                          /* ID number of task for some API calls. Kept same as priority for simplicity. */
+                     &u4_taskStack[APP_TASK_STACK_SIZE - 1],  /* Pointer to highest memory address in stack. */
+                     sizeof(u4_taskStack)/sizeof(OS_STACK),   /* Size of stack in terms of OS_STACK. */
+                     APP_TASK1_PRIO,                          /* Priority of task. */
+                     APP_TASK1_PRIO);                         /* ID number of task for some API calls. Kept same as priority for simplicity. */
   /* Init task 2. */
   u1_OSsch_createTask(&app_task2,                             /* Pointer to function definition. */
-                     &u4_taskStack2[OS_TASK_STACK_SIZE - 1],  /* Pointer to highest memory address in stack. */
-                     sizeof(u4_taskStack2),                   /* Size of stack. */
-                     OS_TASK2_PRIO,                           /* Priority of task. */
-                     OS_TASK2_PRIO);                          /* ID number of task for some API calls. Kept same as priority for simplicity. */
+                     &u4_taskStack2[APP_TASK_STACK_SIZE - 1], /* Pointer to highest memory address in stack. */
+                     sizeof(u4_taskStack2)/sizeof(OS_STACK),  /* Size of stack in terms of OS_STACK. */
+                     APP_TASK2_PRIO,                          /* Priority of task. */
+                     APP_TASK2_PRIO);                         /* ID number of task for some API calls. Kept same as priority for simplicity. */
   /* Init task 3. */
   u1_OSsch_createTask(&app_task3,                             /* Pointer to function definition. */
-                     &u4_taskStack3[OS_TASK_STACK_SIZE - 1],  /* Pointer to highest memory address in stack. */
-                     sizeof(u4_taskStack3),                   /* Size of stack. */
-                     OS_TASK3_PRIO,                           /* Priority of task. */
-                     OS_TASK3_PRIO);                          /* ID number of task for some API calls. Kept same as priority for simplicity. */
+                     &u4_taskStack3[APP_TASK_STACK_SIZE - 1], /* Pointer to highest memory address in stack. */
+                     sizeof(u4_taskStack3)/sizeof(OS_STACK),  /* Size of stack in terms of OS_STACK. */
+                     APP_TASK3_PRIO,                          /* Priority of task. */
+                     APP_TASK3_PRIO);                         /* ID number of task for some API calls. Kept same as priority for simplicity. */
                      
   /* Hand control to OS, will not return. */                   
   vd_OSsch_start();
@@ -85,7 +86,7 @@ static void app_task1(void)
 {
   U4 u4_t_sleepTime;
 
-  u4_t_sleepTime = OS_TASK1_PERIOD;
+  u4_t_sleepTime = APP_TASK1_PERIOD;
   
   while(1)
   {
@@ -106,7 +107,7 @@ static void app_task2(void)
 {
   U4 u4_t_sleepTime;
   
-  u4_t_sleepTime = OS_TASK2_PERIOD;
+  u4_t_sleepTime = APP_TASK2_PERIOD;
 
   while(1)
   {
@@ -127,7 +128,7 @@ static void app_task3(void)
 {
   U4 u4_t_sleepTime;
   
-  u4_t_sleepTime = OS_TASK3_PERIOD;
+  u4_t_sleepTime = APP_TASK3_PERIOD;
 
   while(1)
   {
