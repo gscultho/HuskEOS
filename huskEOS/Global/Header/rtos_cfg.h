bb3aa81b966d142d09454c4607f0735b9a891a6c
Garrett Sculthorpe <51279543+gscultho@users.noreply.github.com>
2020-05-11 10:38:48 -0400

    5_11

--- huskEOS/Global/Header/rtos_cfg.h
+++ huskEOS/Global/Header/rtos_cfg.h
@@ -22,12 +22,12 @@
 #define RTOS_CONFIG_CALC_TASK_CPU_LOAD              (RTOS_CONFIG_FALSE)  /* Can only be enabled if RTOS_CONFIG_BG_TASK and RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP enabled */
 
 /* Scheduling */
-#define RTOS_CONFIG_MAX_NUM_TASKS                   (3)                  /* This number of TCBs will be allocated at compile-time, plus any others used by OS */
-                                                                         /* Available priorities are 0 - 0xEF with 0 being highest priority. */
-#define RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP    (RTOS_CONFIG_TRUE)   /* Can only be used if RTOS_CONFIG_BG_TASK is enabled */
-#define RTOS_CONFIG_ENABLE_STACK_OVERFLOW_DETECT    (RTOS_CONFIG_TRUE)   /* Can only be used if RTOS_CONFIG_BG_TASK is enabled */
-#define RTOS_CONFIG_PRESLEEP_FUNC                   (RTOS_CONFIG_FALSE)  /* If enabled, hook function app_OSPreSleepFcn() can be defined in application. */
-#define RTOS_CONFIG_POSTSLEEP_FUNC                  (RTOS_CONFIG_FALSE)  /* If enabled, hook function app_OSPostSleepFcn() can be defined in application. */
+#define RTOS_CONFIG_MAX_NUM_TASKS                   (3)                   /* This number of TCBs will be allocated at compile-time, plus any others used by OS */
+                                                                          /* Available priorities are 0 - 0xEF with 0 being highest priority. */
+#define RTOS_CONFIG_ENABLE_BACKGROUND_IDLE_SLEEP    (RTOS_CONFIG_TRUE)    /* CPU goes to sleep when idle. */
+#define RTOS_CONFIG_ENABLE_STACK_OVERFLOW_DETECT    (RTOS_CONFIG_TRUE)    /* Check for stack overflow periodically. */
+#define RTOS_CONFIG_PRESLEEP_FUNC                   (RTOS_CONFIG_FALSE)   /* If enabled, hook function app_OSPreSleepFcn() can be defined in application. */
+#define RTOS_CONFIG_POSTSLEEP_FUNC                  (RTOS_CONFIG_FALSE)   /* If enabled, hook function app_OSPostSleepFcn() can be defined in application. */
 
 /* Mailbox */
 #define RTOS_CFG_OS_MAILBOX_ENABLED                 (RTOS_CONFIG_FALSE)
